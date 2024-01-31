ef98237bf89c55ab5d0dffbf5354a2dc8bee0ae9
nunom4chado <nunom4chado@gmail.com>
2024-01-31 11:59:51 +0000

    create main branch

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -54,6 +54,10 @@ std::vector<int> PmergeMe::merge_insertion_sort(std::vector<int> A) {
 
     /* ---------------------------- Step 3: Insertion --------------------------- */
 
+    std::vector<int> main_branch = create_main_branch(pairs, has_stray, stray);
+
+    print_container(main_branch, "inserting...");
+
     return A;
 }
 
@@ -61,12 +65,38 @@ std::vector<int> PmergeMe::merge_insertion_sort(std::vector<int> A) {
 /*                                  Utilities                                 */
 /* -------------------------------------------------------------------------- */
 
+std::vector<int> create_main_branch(std::vector<std::pair<int, int> > &pairs, bool has_stray,
+                                    int stray) {
+    std::vector<int> main_branch;
+    std::vector<std::pair<int, int> >::iterator pairs_it;
+
+    (void)has_stray;
+    (void)stray;
+
+    for (pairs_it = pairs.begin(); pairs_it != pairs.end(); pairs_it++) {
+        if (pairs_it == pairs.begin()) {
+            // first pair (a1 and b1)
+            // since b1 can be inserted immediately, lets do it now
+            main_branch.push_back(pairs_it->second);
+            main_branch.push_back(pairs_it->first);
+        } else {
+            // other b's can't be inserted now! so only add the a's (a2, a3, a4...)
+            main_branch.push_back(pairs_it->first);
+        }
+    }
+
+    return main_branch;
+}
+
 void insert(std::pair<int, int> element, std::vector<std::pair<int, int> > &A, int n) {
     if (n < 0) {
+        // if n is 0 insert it at the begin
         A.insert(A.begin(), element);
     } else if (element.first >= A[n].first) {
+        // if element is bigger or equal to the element in the n position, insert it after
         A.insert(A.begin() + n + 1, element);
     } else {
+        // else try find another place for it in a lower index
         insert(element, A, n - 1);
     }
 }
@@ -94,6 +124,17 @@ void print_pairs(std::vector<std::pair<int, int> > pairs, const std::string &msg
     std::vector<std::pair<int, int> >::iterator it;
     for (it = pairs.begin(); it != pairs.end(); it++) {
         std::cout << "a: " << it->first << " b: " << it->second << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+void print_container(std::vector<int> list, const std::string &msg) {
+    std::cout << msg << std::endl;
+
+    std::vector<int>::iterator it;
+    for (it = list.begin(); it != list.end(); it++) {
+        std::cout << *it << std::endl;
     }
 
     std::cout << std::endl;
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -19,5 +19,8 @@ class PmergeMe {
 
 void print_pairs(std::vector<std::pair<int, int> > pairs, const std::string &msg);
 void recursive_sort_pair(std::vector<std::pair<int, int> > &pairs, int n);
+std::vector<int> create_main_branch(std::vector<std::pair<int, int> > &pairs, bool has_stray,
+                                    int stray);
+void print_container(std::vector<int> list, const std::string &msg);
 
 #endif /* PMERGEME_HPP */
